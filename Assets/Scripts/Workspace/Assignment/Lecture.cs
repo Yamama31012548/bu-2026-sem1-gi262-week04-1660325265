af62e878974bf611079be0b0b1a39ec233117c2f
Yamama3101 <bong31012548@gmail.com>
2026-09-15 20:23:18 +0700

    LectherWeek4

--- Assets/Scripts/Workspace/Assignment/Lecture.cs
+++ Assets/Scripts/Workspace/Assignment/Lecture.cs
@@ -22,7 +22,35 @@ namespace Assignment
 
         public void LCT02_SyntaxLinkedList()
         {
-            throw new System.NotImplementedException();
+            string[] playerNames = new string[1000];
+
+            LinkedList<string> linkedlist = new LinkedList<string>();
+            linkedlist.AddLast("Node 1");
+            linkedlist.AddLast("Node 2");
+            linkedlist.AddFirst("Node 0");
+            LinkedListNode<string> node1 = linkedlist.Find("Node 1");
+            Debug.Log(node1.Value);
+            Debug.Log(node1.Next.Value);
+            Debug.Log(node1.Previous.Value);
+
+            var firstNode = linkedlist.First;
+            var lastNode = linkedlist.Last;
+            Debug.Log(firstNode.Previous);
+            Debug.Log(lastNode.Next);
+
+            linkedlist.AddAfter(node1, "Node 1.5");
+            linkedlist.AddBefore(node1, "Node 0.5");
+
+            linkedlist.RemoveFirst();
+            linkedlist.RemoveLast();
+            linkedlist.Remove("Node 1.5");
+            linkedlist.Clear();
+
+            Debug.Log("-----");
+            foreach(var item in linkedlist)
+            {
+                Debug.Log(item);
+            }
         }
 
         public void LCT03_SyntaxHashTable()
@@ -32,7 +60,31 @@ namespace Assignment
 
         public void LCT04_SyntaxDictionary()
         {
-            throw new System.NotImplementedException();
+            Dictionary<string, int> inv = new Dictionary<string, int>();
+            //var inv = new Dictionary<string, int>();
+
+            inv.Add("Potion", 5);
+            inv.Add("Banana", 1);
+            inv.Add("Apple", 10);
+
+            inv["Apple"] = 0;
+
+            inv["Apple1"] = 1;
+            int potion = inv["Potion"];
+            Debug.Log("potion: " + potion);
+
+            bool hasPotion = inv.ContainsKey("Potion");
+            Debug.Log("hasPotion" + hasPotion);
+
+            inv.Remove("Banana");
+
+            foreach(KeyValuePair<string, int> kvp in inv)
+            {
+                var key = kvp.Key;
+                var value = kvp.Value;
+                Debug.Log($"{key} => {value}");
+            }
+            inv.Clear();
         }
 
         #endregion
